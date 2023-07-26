2d9352a4208d0570ad4cb383ec68187698c91484
aliSalehAbdulhadi <iqalisaleh@gmail.com>
2023-07-26 13:22:19 +0300

    refactor:removed placeholder component and used placeholder in the parent component right away

--- src/components/Cards/CreditsCard/CreditsCard.tsx
+++ src/components/Cards/CreditsCard/CreditsCard.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { v4 as uuidv4 } from 'uuid';
 import LoadingPicture from '../../LoadingComponent/LoadingPicture/LoadingPicture';
-import CreditsCardPlaceholder from '../../Placeholders/CreditsCardPlaceholder/CreditsCardPlaceholder';
 import { imageQualitySmallScreen } from '@/src/global/globalVariables';
 import { charactersLengthHandler } from '@/src/helper/charactersLengthHandler';
+import { RxPerson } from 'react-icons/rx';
 
 const CreditsCard = ({
   data,
@@ -18,69 +18,71 @@ const CreditsCard = ({
   const [loading, setLoading] = useState(true);
 
   return (
-    <CreditsCardPlaceholder
-      data={data}
-      condition={data?.profile_path}
-      mediaType={mediaType}
-    >
-      <div className="transition-all sm:hover:opacity-90">
-        {loading && (
-          <div className="h-[365px]  w-[195px] ">
-            <LoadingPicture />
-          </div>
-        )}
-        <Image
-          quality={imageQualitySmallScreen}
-          src={`https://image.tmdb.org/t/p/original/${data?.profile_path}`}
-          width={150}
-          height={250}
-          alt="Title Image"
-          className={`h-[250px] w-[195px] object-fit rounded-t ${
-            loading === false ? 'opacity-100' : 'opacity-0'
-          }`}
-          onLoad={() => setLoading(false)}
-        />
+    <div>
+      {data.profile_path?.length > 0 ? (
+        <div className="transition-all sm:hover:opacity-90">
+          {loading && (
+            <div className="h-[365px]  w-[195px] ">
+              <LoadingPicture />
+            </div>
+          )}
+          <Image
+            quality={imageQualitySmallScreen}
+            src={`https://image.tmdb.org/t/p/original/${data?.profile_path}`}
+            width={150}
+            height={250}
+            alt="Title Image"
+            className={`h-[250px] w-[195px] object-fit rounded-t ${
+              loading === false ? 'opacity-100' : 'opacity-0'
+            }`}
+            onLoad={() => setLoading(false)}
+          />
+        </div>
+      ) : (
+        <div className="h-[250px] bg-placeholder flex items-center justify-center">
+          <RxPerson className="h-20 w-20 opacity-40 mr-2" />
+        </div>
+      )}
 
-        {data?.jobs || data?.job ? (
-          <div className="px-2 mt-2 flex flex-col w-[100%] h-[7rem]">
-            <span>{charactersLengthHandler(data?.name, 38)}</span>
-            <span className=" text-sm text-white opacity-60  w-full">
-              {mediaType === 'movie'
-                ? data?.job
-                : data?.jobs?.map((job: any) => (
-                    <span key={uuidv4()} className="w-full">
-                      {job?.job}
-                    </span>
-                  ))}
-            </span>
-
-            {mediaType === 'tv' && (
-              <span className="mt-10 text-xs text-white text-opacity-75">
-                {data?.total_episode_count} Episodes
-              </span>
-            )}
-          </div>
-        ) : (
-          <div className="px-2 mt-2 flex flex-col w-[100%] h-[7rem] overflow-auto scrollBar">
+      {data?.jobs || data?.job ? (
+        <div className="px-2 mt-2 flex flex-col w-[100%] h-[7rem]">
+          <span>{charactersLengthHandler(data?.name, 38)}</span>
+          <span className=" text-sm text-white opacity-60  w-full">
             {mediaType === 'movie'
-              ? data?.character
-              : data?.roles?.map((role: any) => (
+              ? data?.job
+              : data?.jobs?.map((job: any) => (
                   <span key={uuidv4()} className="w-full">
-                    {charactersLengthHandler(role?.character, 38)}
+                    {job?.job}
                   </span>
                 ))}
-            <span className=" text-sm opacity-75 w-full">
-              {charactersLengthHandler(data?.name, 38)}
+          </span>
+
+          {mediaType === 'tv' && (
+            <span className="mt-10 text-xs text-white text-opacity-75">
+              {data?.total_episode_count} Episodes
             </span>
-            {mediaType === 'tv' && (
-              <span className="mt-10 mb-3 text-xs text-white text-opacity-75">
-                {data?.total_episode_count} Episodes
-              </span>
-            )}
-          </div>
-        )}
-      </div>
-    </CreditsCardPlaceholder>
+          )}
+        </div>
+      ) : (
+        <div className="px-2 mt-2 flex flex-col w-[100%] h-[7rem] overflow-auto scrollBar">
+          {mediaType === 'movie'
+            ? data?.character
+            : data?.roles?.map((role: any) => (
+                <span key={uuidv4()} className="w-full">
+                  {charactersLengthHandler(role?.character, 38)}
+                </span>
+              ))}
+          <span className=" text-sm opacity-75 w-full">
+            {charactersLengthHandler(data?.name, 38)}
+          </span>
+          {mediaType === 'tv' && (
+            <span className="mt-10 mb-3 text-xs text-white text-opacity-75">
+              {data?.total_episode_count} Episodes
+            </span>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/components/Cards/PosterCard/PosterCard.tsx
+++ src/components/Cards/PosterCard/PosterCard.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
-import PosterCardPlaceholder from '../../Placeholders/PosterCardPlaceholder/PosterCardPlaceholder';
 import LoadingPicture from '../../LoadingComponent/LoadingPicture/LoadingPicture';
 import { imageQualitySmallScreen } from '@/src/global/globalVariables';
 import { charactersLengthHandler } from '@/src/helper/charactersLengthHandler';
+import { SlPicture } from 'react-icons/sl';
 
 const PosterCard = ({
   imageUrl,
@@ -22,51 +22,51 @@ const PosterCard = ({
 }) => {
   const [loading, setLoading] = useState(true);
   return (
-    <PosterCardPlaceholder
-      condition={imageUrl}
-      title={title}
-      rating={rating}
-      releaseDate={releaseDate}
-      mediaType={mediaType}
-    >
-      <div className="transition-all sm:hover:opacity-90">
-        {loading && (
-          <div className="h-[367px]  w-[195px] ">
-            <LoadingPicture />
-          </div>
-        )}
-        <Image
-          quality={imageQualitySmallScreen}
-          src={`https://image.tmdb.org/t/p/original/${imageUrl}`}
-          width={150}
-          height={250}
-          alt="Title Image"
-          className={`h-[250px] w-[195px] object-fit rounded-t ${
-            loading === false ? 'opacity-100' : 'opacity-0'
-          }`}
-          onLoad={() => setLoading(false)}
-        />
-
-        <div className="px-2 flex flex-col w-[195px] h-full rounded-b">
-          <span className="mt-2 h-[4.8rem] w-fit">
-            {charactersLengthHandler(title, 38)}
-          </span>
-          <div className="flex  items-center justify-between opacity-75 w-full text-xs">
-            <div className="flex items-center ">
-              <span className="border-[1px] rounded min-w-[1.5rem] min-h-[1rem] p-1 flex justify-center items-center bg-black bg-opacity-20  border-white border-opacity-75 mr-3 font-averia">
-                {mediaType === 'tv' ? 'TV' : 'Movie'}
-              </span>
-
-              <span>{releaseDate?.split('-')[0]}</span>
+    <div className="transition-all sm:hover:opacity-90">
+      {imageUrl?.length > 0 ? (
+        <div>
+          {loading && (
+            <div className="h-[367px]  w-[195px] ">
+              <LoadingPicture />
             </div>
+          )}
+          <Image
+            quality={imageQualitySmallScreen}
+            src={`https://image.tmdb.org/t/p/original/${imageUrl}`}
+            width={150}
+            height={250}
+            alt="Title Image"
+            className={`h-[250px] w-[195px] object-fit rounded-t ${
+              loading === false ? 'opacity-100' : 'opacity-0'
+            }`}
+            onLoad={() => setLoading(false)}
+          />
+        </div>
+      ) : (
+        <div className="h-[250px] bg-placeholder flex items-center justify-center">
+          <SlPicture className="h-[3.5rem] w-[3.5rem] opacity-40" />
+        </div>
+      )}
 
-            <span className=" text-[.8rem]">
-              {rating === 0 ? 'Not Rated' : `${parseInt(rating)}% Liked`}
+      <div className="px-2 flex flex-col w-[195px] h-full rounded-b">
+        <span className="mt-2 h-[4.8rem] w-fit">
+          {charactersLengthHandler(title, 38)}
+        </span>
+        <div className="flex  items-center justify-between opacity-75 w-full text-xs">
+          <div className="flex items-center ">
+            <span className="border-[1px] rounded min-w-[1.5rem] min-h-[1rem] p-1 flex justify-center items-center bg-black bg-opacity-20  border-white border-opacity-75 mr-3 font-averia">
+              {mediaType === 'tv' ? 'TV' : 'Movie'}
             </span>
+
+            <span>{releaseDate?.split('-')[0]}</span>
           </div>
+
+          <span className=" text-[.8rem]">
+            {rating === 0 ? 'Not Rated' : `${parseInt(rating)}% Liked`}
+          </span>
         </div>
       </div>
-    </PosterCardPlaceholder>
+    </div>
   );
 };
 
